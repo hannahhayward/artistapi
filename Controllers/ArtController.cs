10447254f974c11e61d604ecd869ffcb1af38b3b
hannah hayward <hannahhayward97@gmail.com>
2021-07-14 09:59:14 -0600

    fakedb

--- Controllers/ArtController.cs
+++ Controllers/ArtController.cs
@@ -6,8 +6,8 @@ namespace artistapi.Controllers{
   [Route("api/[controller]")]
   public class ArtController: ControllerBase{
     private readonly ArtService _as;
-    public ArtController(ArtService as){
-      _as = as;
+    public ArtController(ArtService ats){
+      _as = ats;
     }
   }
 }
---
+++ Data/FakeDb.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using artistapi.Models;
+
+namespace artistapi.Data{
+  public static class FakeDb{
+    public static List<Art> Arts = new List<Art>();
+  } 
+}
